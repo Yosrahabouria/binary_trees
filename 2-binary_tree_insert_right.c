b6e5599eed6aee307446770fcd8c872d19cac82e
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-27 03:00:19 -0800

    binary treee

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (node == NULL)
 		return (NULL);
 
-	node->i = value;
+	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
