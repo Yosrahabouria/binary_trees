8882b0ffdc556b99ca44f6eb520f348c2045b3c7
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-27 03:12:21 -0800

    binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (node == NULL)
 		return (NULL);
 
-	node->i = value;
+	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
